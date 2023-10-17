2ec034eba2d0cf9debc61100a85dbc5ea71e0099
KostiantynZhulinas <k.zhulinas@gmai.com>
2023-10-18 00:10:37 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 function summarize(num) {
-    if (num === undefined) {
+    if (typeof num !== 'number') {
       num = 1;
     }
   
-    return function(addend) {
-      return num + addend;
+    return function (x) {
+      if (typeof x !== 'number') {
+        return num + 1;
+      }
+      return x + num;
     };
   }
   
   const addFive = summarize(5);
   console.log(addFive(3));
-  console.log(addFive(6));
-  
+  console.log(addFive());
